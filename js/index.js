f864fd1439994924d931059941a6f00fc3883067
Axel Meyrek Mac <chavezsud1406@gmail.com>
2026-02-16 22:38:39 -0600

    Responsive stiky

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const logo = document.querySelector('.logo');
+let charlas = [];
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
@@ -12,7 +13,6 @@ window.addEventListener('scroll', () => {
 window.addEventListener('scroll', () => {
   const elemento = document.querySelector('.botonera');
   const posicion = elemento.getBoundingClientRect().top;
-  console.log(posicion);
 
   if (posicion < 100) {
     elemento.style.zIndex = "10";
@@ -67,4 +67,11 @@ const scrollObserver = new IntersectionObserver((entries) => {
 }, { threshold: 0.15 });
 
 cards.forEach(card => scrollObserver.observe(card));
+
+
+const consumirCharlas = async () => {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyb0lbfQGKlO0v1NucBaIUfs9HT0eJDICMwSO_9vjnMNStjUnoOtYrUxSZEYKZQQQAnQw/exec');
+    const data = await response.json();
+    console.log(data);
+}
 
